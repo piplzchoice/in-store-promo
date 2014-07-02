536e30a1f997515ee200c47a79fe8f3a9acc5f51
behtea <behtea@gmail.com>
2014-07-02 15:44:00 +0700

    add crud location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,70 @@ class LocationsController < ApplicationController
   before_filter :authenticate_user!
   
   authorize_resource class: LocationsController
+
+  def index
+    @locations = Location.all
+    respond_to do |format|
+      format.html
+    end    
+  end
+
+  def new
+    @location = Location.new
+    respond_to do |format|
+      format.html
+    end    
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+    respond_to do |format|
+      format.html
+    end    
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    respond_to do |format|
+      format.html
+    end    
+  end
+
+  def create
+    @location = Location.new(location_params)
+    respond_to do |format|
+      format.html do
+        if @location.save
+          redirect_to locations_url, notice: "Location created"
+        else
+          render :new
+        end
+      end
+    end       
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    respond_to do |format|
+      format.html do
+        if @location.update_attributes(location_params)
+          redirect_to locations_url, notice: "Location updated"
+        else
+          render :edit
+        end
+      end
+    end
+
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    if @location.destroy
+      redirect_to locations_url, {notice: "Location deleted"}
+    end    
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :address, :city, :state, :zipcode)
+  end    
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
-  has_many :projects
+  # has_many :projects
+  
 end

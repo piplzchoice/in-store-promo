48ea34b94cece9b1d3da8fb3bf6a31975a332750
behtea <behtea@gmail.com>
2014-07-04 22:51:55 +0700

    add email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'instorem.herokuapp.com',
+    user_name:            'ehbeat',
+    password:             'q2w1e4r3t5',
+    authentication:       'plain',
+    enable_starttls_auto: true  }  
 end

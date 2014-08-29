14f7cfb1210c29e27e60c6f74aeb638c5fb4be16
behtea <behtea@gmail.com>
2014-08-29 18:19:23 +0700

    change colour of ba availablty

--- app/models/available_date.rb
+++ app/models/available_date.rb
@@ -39,14 +39,43 @@ class AvailableDate < ActiveRecord::Base
     end
   end
 
-  def get_color
-    if am && pm
-      "#3c763d"
-    elsif am && !pm
-      "#f0ad4e"
-    elsif !am && pm
-      "#428bca" 
+  def get_color    
+    color = nil
+    time_range = availablty.midnight..(availablty.midnight + 1.day)
+    services = self.brand_ambassador.services.where({start_at: time_range})
+
+    if services.blank?
+      if am && pm
+        color = "#3c763d"
+      elsif am && !pm
+        color = "#f0ad4e"
+      elsif !am && pm
+        color = "#428bca"
+      end
+    else
+      service = services.first     
+      period = service.start_at.strftime("%p")
+      if period == "AM"
+        if pm
+          color = "#428bca"
+        end
+      elsif period == "PM"
+        if am
+          color = "#f0ad4e"
+        end
+      end
+      # if service.status == Service.status_cancelled  
+      #   color = "#3c763d" if am || pm
+      # else
+      #   if service.start_at.strftime("%p") == "AM"
+      #     color = "#f0ad4e" if am
+      #   elsif service.start_at.strftime("%p") == "PM"
+      #     color = "#428bca" if am || pm
+      #   end            
+      # end
     end
+
+    return color
   end
 
 end

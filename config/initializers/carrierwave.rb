0d32879e3f28293b20a55624177fc8409aac5f6f
behtea <behtea@gmail.com>
2016-01-30 06:34:32 +0700

    carriwr wve

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => ENV['AWS_REGION']
-  }
-  config.fog_directory  = ENV['AWS_DIRECTORY']
+if Rails.env != "development"
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION']
+    }
+    config.fog_directory  = ENV['AWS_DIRECTORY']
+  end
 end

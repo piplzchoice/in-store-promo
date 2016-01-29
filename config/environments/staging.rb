36b9a41aa5f018d332c4ac53e71422a3b095c17a
behtea <behtea@gmail.com>
2016-01-29 20:13:53 +0700

    add new domain

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'staging.in-store-promo.com' }
+  config.action_mailer.default_url_options = { :host => 'staging.flavorfanaticsism.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '2211184d188e916fa',

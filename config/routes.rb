d9125aa06db6fd8bef75b9af7ac46bd248c8c204
behtea <behtea@gmail.com>
2016-07-17 19:08:09 +0700

    add remove location

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
         post :recurring
         post :archive
         post :add_location
+        delete :remove_location
         delete :removecopy
       end
     end

95be97ea9c25fe7a79603db2cc003e65b3fb740b
behtea <behtea@gmail.com>
2016-07-17 07:15:03 +0700

    add archive button

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       member do
         put :update_status
         post :recurring
+        post :archive
         delete :removecopy
       end
     end
---
+++ db/migrate/20160717001012_add_archive_status_order.rb
@@ -0,0 +1,5 @@
+class AddArchiveStatusOrder < ActiveRecord::Migration
+  def change
+    add_column :orders, :archived, :boolean, default: false
+  end
+end

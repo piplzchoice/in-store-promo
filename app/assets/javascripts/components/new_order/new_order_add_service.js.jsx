6a6c4b86534ddfe7344482d52ec7b5db11b05a66
behtea <behtea@gmail.com>
2016-08-04 17:55:26 +0700

    change text

--- app/assets/javascripts/components/new_order/new_order_add_service.js.jsx
+++ app/assets/javascripts/components/new_order/new_order_add_service.js.jsx
@@ -15,7 +15,7 @@ var NewOrderAddService = React.createClass({
     }
     return(
       <div className="col-md-2">
-        <button className="btn btn-info" style={showBtn} onClick={this.handleShowModal}>Add Service</button>
+        <button className="btn btn-info" style={showBtn} onClick={this.handleShowModal}>Add Demo</button>
         {this.state.view.showModal ?
           <NewOrderModalFormService
               locations={this.props.locations}

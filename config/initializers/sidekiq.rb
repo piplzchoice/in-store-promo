4b33bdfcd8ecac979b80056ea3199acf08c69f16
behtea <behtea@gmail.com>
2016-08-16 17:20:08 +0700

    changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,20 @@
 require 'sidekiq'
 
-db_number = 5
+db_number = 1
 
 case Rails.env
 when "development"
-  db_number = 6
+  db_number = 2
 when "staging"
-  db_number = 7
+  db_number = 3
 when "production"
-  db_number = 8
+  db_number = 4
 end    
 
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => "DW-#{Rails.env}", :url => "redis://127.0.0.1:6379/#{db_number}" }
+  config.redis = { :namespace => "ISM-#{Rails.env}", :url => "redis://127.0.0.1:6379/#{db_number}" }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => "DW-#{Rails.env}", :url => "redis://127.0.0.1:6379/#{db_number}" }
+  config.redis = { :namespace => "ISM-#{Rails.env}", :url => "redis://127.0.0.1:6379/#{db_number}" }
 end
